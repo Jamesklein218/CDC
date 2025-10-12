9c18baa4b87e8aa853dffed517d4979428683d91
Tran Ngoc Dnag Khoa <tndangkhoa218@gmail.com>
2025-10-12 19:03:43 +0700

    chore: add infra

--- TwitchChat.Domain/ServiceCollectionExtensions.cs
+++ TwitchChat.Domain/ServiceCollectionExtensions.cs
@@ -1,17 +1,11 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using TwitchChat.Domain.Services;
 using TwitchChat.Domain.Services.Interfaces;
 
-namespace CDC.Infrastructure;
+namespace CDC.Domain;
 
 public static class ServiceCollectionExtensions
 {
-  public static IServiceCollection AddTwitchChatConfig(this IServiceCollection services, IConfiguration config)
-  {
-    return services;
-  }
-
   public static IServiceCollection AddTwitchChatServices(this IServiceCollection services)
   {
     services.AddScoped<ITwitchChatWorkflowService, TwitchChatWorkflowService>();
---
+++ TwitchChat.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TwitchChat.Infrastructure;
+
+public static class ServiceCollectionExtensions
+{
+  public static IServiceCollection AddTwitchChatConfig(this IServiceCollection services, IConfiguration config)
+  {
+    return services;
+  }
+
+  public static IServiceCollection AddTwitchChatInfrastructures(this IServiceCollection services)
+  {
+    return services;
+  }
+}
